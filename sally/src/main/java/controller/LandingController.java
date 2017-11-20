8e3a1c4da407fa9ea04c86550ee43304928dd80b
JuanDanielCR <castilloreyesjuan@gmail.com>
2017-11-20 14:11:11 -0600

    Controller

--- sally/src/main/java/controller/LandingController.java
+++ sally/src/main/java/controller/LandingController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/sally")
 @Controller
-public class landingController {
+@RequestMapping("/sally")
+public class LandingController {
 	@GetMapping("/inicio")
-	public String landing() {
+	public String inicio() {
 		return "landing";
 	}
 }
