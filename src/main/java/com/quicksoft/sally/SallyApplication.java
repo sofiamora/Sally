1b26c4cc781a7da80aa98136bbcc434377236a65
JuanDanielCR <castilloreyesjuan@gmail.com>
2017-11-20 11:01:55 -0600

    Sally con SpringBoot

---
+++ src/main/java/com/quicksoft/sally/SallyApplication.java
@@ -0,0 +1,12 @@
+package com.quicksoft.sally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SallyApplication.class, args);
+	}
+}
