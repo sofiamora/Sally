9adf5f756bb77334497c6b245c0fdf87e650f605
JuanDanielCR <castilloreyesjuan@gmail.com>
2017-11-20 18:15:00 -0600

    Controllers

--- sally/src/main/java/com/quicksoft/sally/controller/ComprasController.java
+++ sally/src/main/java/com/quicksoft/sally/controller/ComprasController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/dashboard")
-public class DashboardController {
+@RequestMapping("/compras")
+public class ComprasController {
 
 }
